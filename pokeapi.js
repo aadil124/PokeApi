f2e8afa9567161f1a83c1d175240d45ff0809d2a
aadil124 <ansariaadil124@gmail.com>
2023-06-23 22:26:40 +0530

    pokemon number updated

--- pokeapi.js
+++ pokeapi.js
@@ -1,5 +1,4 @@
 document.body.innerHTML = `
-
 <div class="Container bg-warning p-2">
 <div class="heading ">
 <h1 class="text-center text-dark p-4">Pokemon List</h1>
@@ -8,8 +7,9 @@ document.body.innerHTML = `
 </div>
 </div>
 `;
-const pokemon_number = 10;
-const fetchPokemons = async () => {
+
+const pokemon_number = 50;
+const fetchPokemon = async () => {
   for (let i = 1; i <= pokemon_number; i++) {
     getPokemonData(i);
   }
@@ -25,7 +25,7 @@ const getPokemonData = async (id) => {
     console.log(err);
   }
 };
-fetchPokemons();
+fetchPokemon();
 
 const createCardData = (pokemonData) => {
   console.log(pokemonData);
@@ -40,7 +40,6 @@ const createCardData = (pokemonData) => {
     <div class="card mb-3 bg-danger text-white h-100">
     <div class="card-header">
     <img src="${pokeImg}" alt="${pokemonName}">
-   
      <h4 class="card-title item1">Name: ${pokemonName}</h4>
     </div>
     <div class="card-body">
@@ -52,31 +51,3 @@ const createCardData = (pokemonData) => {
     </div>
     `;
 };
-
-// const getBreweriesData = async () => {
-//   const response = await fetch(url);
-//   const data = await response.json();
-//   console.log(data[0].name);
-//   const mainContent = document.querySelector(".mainContent");
-//   let output = "";
-//   data.map((item) => {
-//     output += `
-//      <div class="col-sm-12 col-md-6 col-lg-4 mb-3 mb-sm-3 ">
-//      <div class="card mb-3 bg-warning text-black h-100">
-//     <div class="card-body">
-//         <h5 class="card-title item1">Name: ${item.name}</h5>
-//         <h5 class="card-title item2">Type: ${item.brewery_type}</h5>
-//         <h5 class="card-title item3">City: ${item.city}</h5>
-//         <h5 class="card-title item4">State: ${item.state}</h5>
-//         <h5 class="card-title item4">Country: ${item.country}</h5>
-//         <h5 class="card-title item4">Phone: ${item.phone}</h5>
-//         <a href="${item.website_url}" class="btn btn-outline-danger">Website</a>
-//       </div>
-//     </div>
-//      </div>
-//     `;
-//     mainContent.innerHTML = output;
-//     console.log(item.name);
-//   });
-// };
-// getBreweriesData();
